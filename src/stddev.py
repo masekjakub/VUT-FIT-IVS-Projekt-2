e58907fd04a89ff86570d4e7d5decb9f01c6ace0
MartinTheKiller <93610343+MartinTheKiller@users.noreply.github.com>
2022-04-15 17:35:07 +0200

    Added skript to be profiled and input data

---
+++ src/stddev.py
@@ -0,0 +1,50 @@
+import calcLib
+from sys import stdin
+
+####################
+#### READ INPUT ####
+####################
+rawNumbers=""
+for line in stdin:
+    rawNumbers+=" "+line
+rawNumbers = rawNumbers.replace(",",".") #Correcting floats
+rawNumbers = rawNumbers.split(" ")  #Split input string into a list
+
+numbers=[]
+for num in rawNumbers:
+    num = num.strip()
+    try:
+        num=float(num)
+    except:
+        continue
+    else:
+        numbers.append(num)
+
+N=numbers.__len__()
+if N == 0:
+    print("No numbers were inputed!")
+    exit(1)
+
+#########
+### _ ###
+### X ###
+#########
+avgXi=float(0)
+for x in numbers:
+    avgXi = calcLib.add(avgXi,x)
+avgXi = calcLib.div(avgXi,N)
+
+#################
+### DEVIATION ###
+#################
+deviation=0
+
+#sum Xi^2
+for x in numbers:
+    deviation+=calcLib.pwr(x,2)
+
+deviation=calcLib.sub(deviation , calcLib.mul( N , calcLib.pwr(avgXi,2) ) )
+deviation=calcLib.div(deviation, calcLib.sub(N,1) )
+deviation=calcLib.root(deviation,2)
+
+print(deviation)
