dd21ea83bbb755767d4cd2012ad73c3690f9cd34
masekjakub <masekjak02@gmail.com>
2022-04-23 21:48:03 +0200

    calculator.py added doxygen comments

--- src/calculator.py
+++ src/calculator.py
@@ -1,26 +1,43 @@
+
+"""
+Project name: VUT FIT IVS 2. Project
+File: calculator.py
+Date: 23.4.2022
+Authors: Jakub Mašek xmasek19
+Description: Main file of calculator app
+"""
+
+##
+# @file calculator.py
+# @brief Main file of calculator app
+# @author Jakub Mašek
+
 from PyQt5 import QtWidgets
 import re
 import calcLib
 
+## Main class of calculator
 class Ui_calculator(object):
+    #import ui classes
     from ui import setupUi, retranslateUi
     
-    #add symbol from pushed button to inputs cursor position
-    def addSymbolToInput(self, num):
+    ## Function that adds symbol to input line.
+    # @param symbol Symbol to be added
+    def addSymbolToInput(self, symbol):
         curText = self.lineInput.text()
         cursorPos = self.lineInput.cursorPosition()
 
-        curText = curText[0:cursorPos] + str(num) + curText[cursorPos:]
+        curText = curText[0:cursorPos] + str(symbol) + curText[cursorPos:]
         self.lineInput.setText(curText)
         self.lineInput.setFocus()
-        self.lineInput.setCursorPosition(cursorPos + len(str(num)))
+        self.lineInput.setCursorPosition(cursorPos + len(str(symbol)))
 
-    #clear whole input line
+    ## Function that clears input line.
     def clear(self):
         self.lineInput.setText("")
         self.lineInput.setFocus()
 
-    #delete one character before cursor positioin
+    ## Function deletes character ahead of cursor position
     def backspace(self):
         curText = self.lineInput.text()
         cursorIndex = self.lineInput.cursorPosition()-1
@@ -32,18 +49,20 @@ class Ui_calculator(object):
         self.lineInput.setFocus()
         self.lineInput.setCursorPosition(cursorIndex)
 
-    #adds previous result to input line
+    ## Function that adds previous result to input line.
     def ansToInput(self):
         curText = self.lineInput.text()
         self.lineInput.setText(curText + str(self.res))
         self.lineInput.setFocus()
 
-    #deletes whole history label
+    ## Function that clears history.
     def delHistory(self):
         self.textDisplay.clear()
         self.lineInput.setFocus()
 
-    #splits expression by operators and special characters
+    ## Function splits expression by operators and special characters, validates expression and makes necessary changes in it. 
+    # @param expression Math expression to be splitted
+    # @return Returns splitted expression as array
     def splitExprToArr(self, expression):
             expArr = re.split(r'(\+|-|x|\*|/|!|\^|√|π|ln)', expression)
             
@@ -62,20 +81,29 @@ class Ui_calculator(object):
                     if expArr[index+1].isnumeric():
                         expArr[index] = str(expArr[index]) + str(expArr.pop(index+1))
 
+                #if number is next to pi(on right side), adds mul between it
                 if i == "π" and not len(expArr)-index-1 == 0 and not re.match(r'(\+|-|x|\*|/|!|\^)',expArr[index+1]):
                     expArr.insert(index+1,"x")
 
+                #if number is next to pi(on left side), adds mul between it
                 if i == "π" and index > 0 and not re.fullmatch(r'(\+|-|x|\*|/|!|\^|√|π|ln)',expArr[index-1]):
                         expArr.insert(index,"x")
             return expArr
 
+    ## Function removes values on specific indexes from array
+    # @param arr Array
+    # @param indexes Indexes of array to be removed
+    # @return Returns array with removed indexes
     def removeFromArr(self,arr, indexes):
         for index in indexes:
             arr.pop(int(index))
         indexes.clear()
         return arr
 
-    def processLvl1(self, expArr):
+    ## Function processes operation with highest priority
+    # @param expArr Array to be processed
+    # @return Returns array with processed operations
+    def processPriority1(self, expArr):
         for index,i in enumerate(expArr):  
             if i == "!":
                 expArr = self.processOneOperand(expArr, index, "fac", index-1)
@@ -85,7 +113,10 @@ class Ui_calculator(object):
                 expArr = self.processNoOperand(expArr, index, "pi")
         return expArr
 
-    def processLvl2(self, expArr):
+    ## Function processes operation with 2nd highest priority
+    # @param expArr Array to be processed
+    # @return Returns array with processed operations
+    def processPriority2(self, expArr):
         for index,i in enumerate(expArr):   
             if i == "ln":
                 expArr = self.processOneOperand(expArr, index, "ln", index+1)
@@ -106,7 +137,10 @@ class Ui_calculator(object):
                 self.indexesToRemove.insert(0,index)
         return expArr
 
-    def processLvl3(self, expArr):
+    ## Function processes operation with 3rd highest priority
+    # @param expArr Array to be processed
+    # @return Returns array with processed operations
+    def processPriority3(self, expArr):
         for index,i in enumerate(expArr):
             if i == "x" or i == "*":
                 expArr = self.processTwoOperands(expArr, "mul", index-1,index+1)
@@ -118,7 +152,10 @@ class Ui_calculator(object):
                 self.indexesToRemove.insert(0,index)
         return expArr
 
-    def processLvl4(self, expArr):
+    ## Function processes operation with 4th highest priority
+    # @param expArr Array to be processed
+    # @return Returns array with processed operations
+    def processPriority4(self, expArr):
         for index,i in enumerate(expArr):
             if i == "+":
                 expArr = self.processTwoOperands(expArr, "add", index-1,index+1)
@@ -130,7 +167,14 @@ class Ui_calculator(object):
                 self.indexesToRemove.insert(0,index)
         return expArr
 
+    ## Function processes whole expression
+    # @param expArr Array to be processed
+    # @return Returns array with processed operations
     def calculate(self,key):
+        ## Stores last result
+        self.res=""
+        ## Stores indexes of array to be deleted
+        self.indexesToRemove=[]
         try:
             expression = self.lineInput.text()
             expArr = self.splitExprToArr(expression)
@@ -142,68 +186,90 @@ class Ui_calculator(object):
                 return
 
             #process operations with highest priority
-            expArr = self.processLvl1(expArr)
+            expArr = self.processPriority1(expArr)
             expArr = self.removeFromArr(expArr, self.indexesToRemove)
 
             #if first character is + or -, insert 0 ahead
             if expArr[0]  == "-" or expArr[0]  == "+":
                 expArr.insert(0,"0")
+
             #process operations with 2nd highest priority
-            expArr = self.processLvl2(expArr)
+            expArr = self.processPriority2(expArr)
             expArr = self.removeFromArr(expArr, self.indexesToRemove)
             
             #process operations with 3nd highest priority
-            expArr = self.processLvl3(expArr)
+            expArr = self.processPriority3(expArr)
             expArr = self.removeFromArr(expArr, self.indexesToRemove)
 
             #process operations with 4rd highest priority
-            expArr = self.processLvl4(expArr)
+            expArr = self.processPriority4(expArr)
             expArr = self.removeFromArr(expArr, self.indexesToRemove)
             
             #more then 1 node in array means something is not processed
             if len(expArr) != 1:
                 raise SyntaxError()
             
-            print(expArr)
+            print(expArr)#debug REMOVE
+            #eval whole expression and round result
             self.res = eval(expArr[0])
             self.res = (f'{self.res:.15f}')
             self.res = self.res.rstrip('0')
             self.res = self.res.rstrip('.')
-            self.resString = expression + '        =       ' + str(self.res)
+            resString = expression + '        =       ' + str(self.res)
 
         except TypeError as e: 
-            self.resString=repr(e)
+            resString=repr(e)
 
         except ZeroDivisionError as e:
-            self.resString = "Division by zero!"
+            resString = "Division by zero!"
 
         except Exception as e:
-            self.resString = "Wrong syntax!"
+            resString = "Wrong syntax!"
 
         else:
             self.lineInput.setText(str(self.res))
             self.lineInput.setFocus()
 
-        self.textDisplay.append(self.resString)
+        #add result to history and ensure history text label is scrolled down
+        self.textDisplay.append(resString)
         self.textDisplay.ensureCursorVisible()
 
+    ## Function processes operation with 2 operands, inserts result to the greatest index used
+    # @param expArr Array to be processed
+    # @param operation Name of operation to be precessed
+    # @param operand1 Index of first operand
+    # @param operand2 Index of second operand
+    # @return Returns array with processed operation
     def processTwoOperands(self, expArr, operation, operand1, operand2):
         expr = 'calcLib.'+operation+'('+expArr[operand1]+','+expArr[operand2]+')'
         lastIndex = max(operand1, operand2)
         expArr[lastIndex] = expr
         return expArr
 
+    ## Function processes operation with 1 operand, inserts result to the greatest index used
+    # @param expArr Array to be processed
+    # @param operationIndex Index of operation
+    # @param operation Name of operation to be precessed
+    # @param operand Index of operand
+    # @return Returns array with processed operation
     def processOneOperand(self, expArr, operationIndex, operation, operand):
         expr = 'calcLib.'+operation+'('+expArr[operand]+')'
         lastIndex = max(operand, operationIndex)
         expArr[lastIndex] = expr
         return expArr
 
+    ## Function processes operation with no operand, inserts result to the greatest index used
+    # @param expArr Array to be processed
+    # @param operationIndex Index of operation
+    # @param operation Name of operation to be precessed
+    # @param operand Index of operand
+    # @return Returns array with processed operation
     def processNoOperand(self, expArr, operationIndex, operation):
         expr = 'calcLib.'+operation+'()'
         expArr[operationIndex] = expr
         return expArr
 
+## Main function
 if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
