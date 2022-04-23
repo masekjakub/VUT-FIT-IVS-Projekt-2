cd5c9129e5ade605b384042c13fcda451ac3412e
MartinTheKiller <93610343+MartinTheKiller@users.noreply.github.com>
2022-04-23 14:49:14 +0200

    Doxygen for profiling.py, test_calcLib.py

--- src/test_calcLib.py
+++ src/test_calcLib.py
@@ -1,10 +1,29 @@
+"""
+Project name: VUT FIT IVS 2. Project
+File: test_calcLib.py
+Date: 22.4.2022
+Authors: Martin Zelenák xzelen27
+Description: Test suit for testing the calcLib library
+"""
+
+##
+# @file test_calcLib.py
+# @brief Test suit for testing the calcLib library
+# @author Martin Zelenák
+
 import pytest
 import calcLib
 from cmath import pi
 
+## Class of tests for testing basic functions
+# @brief Class of tests for testing calcLib.add, calcLib.sub, calcLib.mul, calcLib.div
 class Test_Basic:
 
     #ADDITION
+    ## Tests for testing calcLib.add
+    # @param x First number to be added
+    # @param y Second number to be added
+    # @param z Result of addition
     @pytest.mark.parametrize("x,y,z",[(1,2,3),
                                       (1,0,1),
                                       (0,0,0),
@@ -16,6 +35,10 @@ class Test_Basic:
         assert calcLib.add(y,x) == z
     
     #SUBTRACTION
+    ## Tests for testing calcLib.sub
+    # @param x Number to be subctracted from
+    # @param y Number to substract
+    # @param z Result of substraction
     @pytest.mark.parametrize("x,y,z",[(2,1,1),
                                       (1,0,1),
                                       (0,0,0),
@@ -27,6 +50,10 @@ class Test_Basic:
         assert calcLib.sub(y,x) == -z
     
     #MULTIPLICATION
+    ## Tests for testing calcLib.mul
+    # @param x Number to multiply
+    # @param y Number to be multiplied by
+    # @param z Result of multiplication
     @pytest.mark.parametrize("x,y,z",[(2,2,4),
                                       (5,0,0),
                                       (2,-2,-4),
@@ -38,6 +65,10 @@ class Test_Basic:
         assert calcLib.mul(y,x) == z
 
     #DIVISION
+    ## Tests for testing calcLib.div
+    # @param x Number to divide
+    # @param y Number to be divided by
+    # @param z Result of division
     @pytest.mark.parametrize("x,y,z",[(6,3,2),
                                       (0,3,0),
                                       (-6,2,-3),
@@ -49,14 +80,17 @@ class Test_Basic:
         if x!=0:
             assert "{0:.14f}".format(calcLib.div(y,x)) == "{0:.14f}".format(1/z)
     
+    ## Tests for testing calcLib.div exception ZeroDivisionError
     def test_division_zero(self):
         with pytest.raises(ZeroDivisionError):
             calcLib.div(1,0)
 
-
-class Test_advanced:
+## Class of tests for testing advanced functions
+# @brief Class of tests for testing calcLib.fac, calcLib.pwr, calcLib.root, calcLib.ln, calcLib.pi
+class Test_Advanced:
     
     #FACTORIAL
+    ## Tests for testing calcLib.fac and exceptions
     def test_factorial(self):
         assert calcLib.fac(10) == 3628800
         assert calcLib.fac(15) == 1307674368000
@@ -70,6 +104,10 @@ class Test_advanced:
             calcLib.fac(-2.963)
     
     #POWER
+    ## Tests for testing calcLib.div
+    # @param x Base number
+    # @param n Exponent
+    # @param y Result of exponentiation
     @pytest.mark.parametrize("x,n,y",[(2,2,4),
                                       (5,1,5),
                                       (32,0,1),
@@ -85,6 +123,10 @@ class Test_advanced:
         assert "{0:.15f}".format(calcLib.pwr(x,n)) == "{0:.15f}".format(y)
     
     #ROOT
+    ## Tests for testing calcLib.root
+    # @param x Number to find the root of
+    # @param n N-th root
+    # @param y Result of division
     @pytest.mark.parametrize("x,n,y",[(4,2,2),
                                       (27,3,3),
                                       (0,35,0),
@@ -94,6 +136,9 @@ class Test_advanced:
     def test_root(self,x,n,y):
         assert "{0:.15f}".format(calcLib.root(x,n)) == "{0:.15f}".format(y)
     
+    ## Tests for testing calcLib.root exceptions
+    # @param x Number to find the root of
+    # @param n N-th root
     @pytest.mark.parametrize("x,n",[(-1,-2),
                                     (5,0),
                                     (-2,4),
@@ -104,6 +149,9 @@ class Test_advanced:
             calcLib.root(x,n)
 
     #NATURAL LOGARITHM
+    ## Tests for testing calcLib.ln
+    # @param x Number given to logarithm
+    # @param y Result of logarithm
     @pytest.mark.parametrize("x,y",[(1,0.0),
                                     (2,0.6931471805599453),
                                     (3,1.0986122886681096),
@@ -116,11 +164,14 @@ class Test_advanced:
     def test_ln(self,x,y):
         assert "{0:.15}".format(calcLib.ln(x)) == "{0:.15}".format(y)
     
+    ## Tests for testing calcLib.ln exceptions
+    # @param x Number given to logarithm
     @pytest.mark.parametrize("x",[0,-1,-2,-500,-0.1,-0.5,-0.0000003])
     def test_ln_exception(self,x):
         with pytest.raises(TypeError):
             calcLib.ln(x)
     
     #PI
+    ## Test for testing calcLib.pi
     def test_pi(self):
         assert "{0:.15f}".format(calcLib.pi()) == "{0:.15f}".format(pi)
