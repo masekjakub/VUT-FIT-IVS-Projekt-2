c91bded8c440e3cf356ac840267b8b5ebe24faf8
Vojtěch Kuchař <93602249+Woytheek@users.noreply.github.com>
2022-04-27 14:42:37 +0200

    added license into test_calcLib.py

--- src/test_calcLib.py
+++ src/test_calcLib.py
@@ -11,6 +11,16 @@ Description: Test suit for testing the calcLib library
 # @brief Test suit for testing the calcLib library
 # @author Martin Zelenák
 
+"""
+This file is part of CubiCulator.
+
+CubiCulator is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+CubiCulator is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with Foobar. If not, see <https://www.gnu.org/licenses/>.
+"""
+
 import pytest
 import calcLib
 from cmath import pi
