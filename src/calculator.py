42a33010237c0cef31506ff39650fe9cf6b12cac
Vojtěch Kuchař <93602249+Woytheek@users.noreply.github.com>
2022-04-27 14:54:21 +0200

    added license into calculator.py

--- src/calculator.py
+++ src/calculator.py
@@ -11,6 +11,13 @@ Description: Main file of calculator app
 # @brief Main file of calculator app
 # @author Jakub Mašek xmasek19
 
+"""
+This file is part of CubiCulator.
+CubiCulator is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+CubiCulator is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+You should have received a copy of the GNU General Public License along with Foobar. If not, see <https://www.gnu.org/licenses/>.
+"""
+
 from PyQt5 import QtWidgets
 import re
 import calcLib
